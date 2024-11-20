f4d60957cc20f29b461e2c766f6522c7cefc62da
City7g <City7gor@gmail.com>
2024-11-20 22:13:36 +0200

    fix ts error

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ document.querySelectorAll('a').forEach((link) => {
   link.addEventListener('click', (e) => {
     e.preventDefault()
 
-    const href = link.getAttribute('href')
+    const href = link.getAttribute('href') as string
 
     document.querySelectorAll('.item').forEach((block) => {
       block.classList.remove('show')
     })
 
     if (document.querySelector(href)) {
-      document.querySelector(href).classList.add('show')
+      document.querySelector(href)!.classList.add('show')
     }
   })
 })
