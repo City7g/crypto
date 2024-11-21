3d5e1d66cf2df60f817b8f8c70d9ca4b2c80ae25
City7g <City7gor@gmail.com>
2024-11-21 20:32:03 +0200

    remove startViewTransition

--- src/about.ts
+++ src/about.ts
@@ -1,10 +1,10 @@
 setTimeout(() => {
-  document.startViewTransition(() => {
-    const wrap = document.querySelector('.popup__wrap') as HTMLElement
-    wrap.style.opacity = '0'
-    document.querySelector('.popup')?.classList.add('loaded')
-    setTimeout(() => {
-      wrap.style.opacity = ''
-    }, 1000)
-  })
+  // document.startViewTransition(() => {
+  const wrap = document.querySelector('.popup__wrap') as HTMLElement
+  wrap.style.opacity = '0'
+  document.querySelector('.popup')?.classList.add('loaded')
+  setTimeout(() => {
+    wrap.style.opacity = ''
+  }, 1000)
+  // })
 }, 2000)
